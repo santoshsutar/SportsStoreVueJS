20f91bfc4b143073ebdbb5d40e4a7c3de1e3a106
Santosh Sutar <santosutar@gmail.com>
2019-01-16 11:01:00 +0530

    Sports Store VueJS

--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import store from "./store";
+import router from "./router";
+import Vuelidate from "vuelidate";
+Vue.filter("currency", (value) => new Intl.NumberFormat("en-US",
+  { style: "currency", currency: "INR" }).format(value));
+  Vue.use(Vuelidate);
 new Vue({
   render: h => h(App),
+  store,
+  router
+
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Store from "../components/Store";
+import ShoppingCart from "../components/ShoppingCart";
+import Checkout from "../components/Checkout";
+import OrderThanks from "../components/OrderThanks";
+import Authentication from "../components/admin/Authentication";
+import Admin from "../components/admin/Admin";
+import dataStore from "../store";
+import ProductAdmin from "../components/admin/ProductAdmin";
+import OrderAdmin from "../components/admin/OrderAdmin";
+import ProductEditor from "../components/admin/ProductEditor";
+Vue.use(VueRouter);
+export default new VueRouter({
+    mode: "history",
+    routes: [
+        { path: "/", component: Store },
+        { path: "/cart", component: ShoppingCart },
+        { path: "/checkout", component: Checkout },
+        { path: "/thanks/:id", component: OrderThanks },
+        { path: "/login", component: Authentication },
+        {
+            path: "/admin", component: Admin,
+            beforeEnter(to, from, next) {
+                if (dataStore.state.auth.authenticated) {
+                    next();
+                } else {
+                    next("/login");
+                }
+            },
+            children: [
+                {
+                    path: "products/:op(create|edit)/:id(\\d+)?",
+                    component: ProductEditor
+                },
+                { path: "products", component: ProductAdmin },
+                { path: "orders", component: OrderAdmin },
+                { path: "", redirect: "/admin/products" }
+            ]
+        },
+        { path: "*", redirect: "/" }
+    ]
+})
